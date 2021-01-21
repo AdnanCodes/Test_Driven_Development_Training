68f9d0fe2ccb672958e5c9bea52f409bb910ed81
Adnan <adnan.chowdhury@outlook.com>
2021-01-21 11:52:44 -0600

    passing two tests

--- src/letter-count.test.js
+++ src/letter-count.test.js
@@ -7,4 +7,9 @@ describe("getLetterCount - basic functionaility", () => {
     const actual = getLetterCount("");
     expect(actual).to.deep.equal(expected);
   });
+  it("return the correct letter count for a word for with only one for each letter", () => {
+    const expected = { c: 1, a: 1, t: 1 };
+    const actual = getLetterCount("cat");
+    expect(actual).to.deep.equal(expected);
+  });
 });
